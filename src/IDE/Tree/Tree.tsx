b124e071d739d8e88e6f5f4d50be0d3986e93cd5
nin <ninian138@gmail.com>
2017-08-18 19:04:28 +0900

    use reset css

--- src/IDE/Tree/Tree.tsx
+++ src/IDE/Tree/Tree.tsx
@@ -20,7 +20,9 @@ export default class Tree extends React.Component<Props, {}> {
     const nodes = this.props.value.node.map( (v, i) => { return (<div key={i}>{v!!.tag}</div>)});
     return (
         <section className="c-tree">
-          <h1 onDragEnter={() => {console.log("enter")}} onDrop={ () => {console.log("www")} }>Tree</h1>
+          <div className="c-tree--head">
+            <h1 onDragEnter={() => {console.log("enter")}} onDrop={ () => {console.log("www")} }>Tree</h1>
+          </div>
           <div className="c-tree--main" data-treeId="root">
             {nodes}
           </div>
