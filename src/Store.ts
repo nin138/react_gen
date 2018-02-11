a5467bc83c356d768c428540c6009a5520214d13
nin <ninian138@gmail.com>
2018-02-12 01:36:57 +0900

    add logger

--- src/Store.ts
+++ src/Store.ts
@@ -1,4 +1,4 @@
-import {createStore, combineReducers, Action} from 'redux'
+import {applyMiddleware ,createStore, combineReducers, Action} from 'redux'
 import edit, {EditAction, EditState} from "./IDE/Edit/Modules";
 import tree, {TreeAction, TreeState} from "./IDE/Tree/Modules";
 import palette, {PaletteAction, PaletteState} from "./IDE/Palette/Modules";
@@ -6,6 +6,8 @@ import ide, {IDEAction, IDEState} from "./IDE/Modules";
 import log, {LogAction, LogState} from "./IDE/Log/Modules";
 import project, {ProjectAction, Project} from "./IDE/Project/Modules";
 import modal, {ModalAction, ModalState} from "./Modal/Modules";
+import {loggerMiddleware} from "./middleware";
+
 
 export default createStore(
     combineReducers({
@@ -16,7 +18,8 @@ export default createStore(
       log,
       project,
       modal,
-    })
+    }),
+    applyMiddleware(loggerMiddleware)
 )
 
 export type AppState = {
---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { Middleware, MiddlewareAPI, Dispatch, Action } from "redux";
+import {AppState} from "./Store";
+export interface ExtendedMiddleware<StateType> extends Middleware {
+  <S extends StateType>(api: MiddlewareAPI<S>): (next: Dispatch<S>) => Dispatch<S>;
+}
+
+export const loggerMiddleware: ExtendedMiddleware<AppState> = <S extends AppState>(api: MiddlewareAPI<S>) =>
+    (next: Dispatch<S>) =>
+        <A extends Action>(action: A): A => {
+          // Can use: api.getState()
+          console.log(action);
+          return next(action);
+        };
